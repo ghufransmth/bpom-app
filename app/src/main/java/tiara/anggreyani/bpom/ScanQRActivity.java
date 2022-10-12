467f751f0c38d7ed44c3cc90ccb1e8cd427d56b7
Ghufran <franssmith7@gmail.com>
2022-10-12 21:08:11 +0700

    added qr scan functional, button verified and etc.

---
+++ app/src/main/java/tiara/anggreyani/bpom/ScanQRActivity.java
@@ -0,0 +1,187 @@
+package tiara.anggreyani.bpom;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ScanQRActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.profile_btn_back)
+    ImageView profile_btn_back;
+
+    private CodeScanner mCodeScanner;
+    String getPickupDetailId="", getRackCode="";
+    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_qr);
+
+        ButterKnife.bind(this);
+
+        profile_btn_back.setOnClickListener(this);
+
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(result.getText().isEmpty()){
+                            Toast.makeText(ScanQRActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                        }else{
+                            if(count > 0){
+                                Toast.makeText(ScanQRActivity.this, "Only once scan", Toast.LENGTH_SHORT).show();
+                            }else{
+                                count += 1;
+                                sendScanUnit(result.getText());
+                            }
+
+                        }
+
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dexter.withActivity(ScanQRActivity.this)
+                        .withPermissions(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .withListener(new MultiplePermissionsListener() {
+                            @Override
+                            public void onPermissionsChecked(MultiplePermissionsReport report) {
+                                if (report.areAllPermissionsGranted()) {
+                                    mCodeScanner.startPreview();
+                                } else {
+                                    // TODO - handle permission denied case
+                                }
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                                token.continuePermissionRequest();
+                            }
+                        }).check();
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.profile_btn_back:
+                finish();
+                break;
+
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Dexter.withActivity(ScanQRActivity.this)
+                .withPermissions(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted()) {
+                            mCodeScanner.startPreview();
+                        } else {
+                            // TODO - handle permission denied case
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if(requestCode == 1){
+                Intent intent = new Intent();
+                setResult(Activity.RESULT_OK,intent);
+                finish();
+            }
+
+        }
+    }
+
+    public void sendScanUnit(String getScanSN){
+        startActivity(new Intent(ScanQRActivity.this, ScanDetailActivity.class));
+    }
+
+
+    public void dialogWarning(String message)
+    {
+        // @M - showing the warning dialog in old style dialog
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_warning_1button);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        TextView textMessage = dialog.findViewById(R.id.teks);
+        textMessage.setText(""+message);
+
+        Button oke = dialog.findViewById(R.id.ok);
+        oke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                mCodeScanner.startPreview();
+            }
+        });
+        dialog.show();
+    }
+}
